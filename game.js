3d5e38c8e35eed1239f3d36402c896e6bc7bad8f
Adam Parness <101610613+AdamParness@users.noreply.github.com>
2024-10-01 13:13:22 -0700

    Computer and Human Logic Implemented
    Added the computer function and human function to pick rps, and built the play round function

---
+++ game.js
@@ -0,0 +1,78 @@
+function getComputerChoice() {
+    //rand picks a random number from 0 to 2
+    let rand = Math.floor(Math.random() * 3);
+    let choice;
+    if (rand == 0){
+        choice = 'rock'
+    }
+    else if (rand == 1){
+        choice = 'paper'
+    }
+    else if (rand == 2){
+        choice = 'scissors'
+    }
+    return choice;
+}
+
+let choice = getComputerChoice()
+//console.log(choice)
+
+function getHumanChoice() {
+    let humanChoice;
+  
+    do {
+      humanChoice = prompt("Pick rock, paper, or scissors").toLowerCase();
+    } while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors");
+  
+    return humanChoice;
+  }
+
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice == 'rock'){
+        if (computerChoice == 'scissors'){
+            console.log("You win! Rock beats Scissors"); 
+            humanScore++;
+        }
+        else if (computerChoice == 'paper'){
+            console.log("You lose. Rock loses to Paper");
+            computerScore++;
+        }
+        else{
+            console.log("Tie");
+        }
+    }
+    else if(humanChoice == 'paper'){
+        if (computerChoice == 'rock'){
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        }
+        else if (computerChoice == 'scissors'){
+            console.log("You lose. Paper loses to Scissors");
+            computerScore++;
+        }
+        else{
+            console.log("Tie"); 
+        }
+    }
+    else if (humanChoice == 'scissors'){
+        if (computerChoice == 'paper'){
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+        }
+        else if (computerChoice == 'rock'){
+            console.log("You lose. Scissors loses to Rock");
+            computerScore++;
+        }
+        else {
+            console.log("Tie");
+        }
+    }
+}
+
+
+
